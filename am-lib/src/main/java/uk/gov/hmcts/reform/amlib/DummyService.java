b59eb1cf0405c1b0b666660e32e4bdbc6319ce65
Kamil Zielinski <K.Zielinski@kainos.com>
2019-01-23 15:19:13 +0100

    Added simple connection to DB with one select statement

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/DummyService.java
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/DummyService.java
@@ -1,8 +1,23 @@
 package uk.gov.hmcts.reform.amlib;
 
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
 public class DummyService {
+    private Jdbi jdbi;
+
+    public DummyService(String dbUrl, String user, String password) {
+        jdbi = Jdbi.create(dbUrl,user, password);
+    }
 
     public String getHello() {
-        return "Hello Dummy Service 2";
+
+        List<Integer> ids = jdbi.withHandle(handle ->
+                handle.createQuery("SELECT access_control_id FROM access_control")
+                .mapTo(Integer.class)
+                .list());
+
+        return "Hello Dummy Service 2 " + ids.size();
     }
 }

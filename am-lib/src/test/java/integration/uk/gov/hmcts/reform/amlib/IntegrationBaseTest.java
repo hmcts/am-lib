975edde4b897add68f9545af7ea598cb9b2ebdd3
Kamil Zielinski <K.Zielinski@kainos.com>
2019-01-29 14:20:06 +0100

    Added reset h2 db after each test

--- am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/IntegrationBaseTest.java
+++ am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/IntegrationBaseTest.java
@@ -3,6 +3,7 @@ package integration.uk.gov.hmcts.reform.amlib;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.jdbi.v3.core.Jdbi;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import uk.gov.hmcts.reform.amlib.AccessManagementService;
@@ -14,6 +15,7 @@ public abstract class IntegrationBaseTest {
 
     // According to H2 docs DB_CLOSE_DELAY is required in order to keep open connection to db (on close, h2 drops db)
     public static final String JDBC_URL = "jdbc:h2:mem:test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1";
+    private static final String H2_BACKUP_LOCATION = "/tmp/h2backup.sql";
 
     protected AccessManagementService ams;
     protected static Jdbi jdbi;
@@ -23,6 +25,7 @@ public abstract class IntegrationBaseTest {
         jdbi = Jdbi.create(JDBC_URL, "sa", "");
 
         initSchema();
+        createBackup();
     }
 
     @Before
@@ -39,5 +42,17 @@ public abstract class IntegrationBaseTest {
         Flyway flyway = new Flyway(configuration);
         int noOfMigrations = flyway.migrate();
         assertThat(noOfMigrations).isGreaterThan(0);
+    }
+
+    private static void createBackup() {
+        jdbi.withHandle(handle -> handle.execute("SCRIPT TO ?", H2_BACKUP_LOCATION));
+    }
+
+    @After
+    public void loadFromBackup() {
+        jdbi.withHandle(handle -> {
+            handle.execute("DROP ALL OBJECTS");
+            return handle.execute("RUNSCRIPT FROM ?", H2_BACKUP_LOCATION);
+        });
     }
 }

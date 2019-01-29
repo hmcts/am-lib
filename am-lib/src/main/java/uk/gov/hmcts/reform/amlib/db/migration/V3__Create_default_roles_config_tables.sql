5b250908db77aa589914faed9d18c4909824c8be
Patryk Jar <jar.patryk@gmail.com>
2019-01-29 16:20:14 +0100

    AM-58: added scripts to create default roles config tables

---
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/db/migration/V3__Create_default_roles_config_tables.sql
@@ -0,0 +1,45 @@
+CREATE TABLE "Roles" (
+  "roleName" varchar(100) PRIMARY KEY,
+  "roleType" VARCHAR (50) NOT NULL,
+  "securityClassification" VARCHAR (10) NOT NULL
+);
+
+CREATE TABLE "Services" (
+  "serviceName" varchar(100) PRIMARY KEY,
+  "serviceDescription" varchar(250)
+);
+
+CREATE TABLE "Resources" (
+  "serviceName" varchar(100) NOT NULL,
+  "resourceType" varchar(100) NOT NULL,
+  "resourceName" varchar(100) NOT NULL,
+  PRIMARY KEY ("serviceName", "resourceType", "resourceName"),
+  CONSTRAINT "Resources_serviceName_fkey" FOREIGN KEY ("serviceName")
+    REFERENCES "Services" ("serviceName") MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE "ResourceAttributes" (
+  "serviceName" varchar(100) NOT NULL,
+  "resourceType" varchar(100) NOT NULL,
+  "resourceName" varchar(100) NOT NULL,
+  "attribute" varchar(250) NOT NULL,
+  "defaultSecurityClassification" VARCHAR (10) NOT NULL,
+  PRIMARY KEY ("serviceName", "resourceType", "resourceName", "attribute"),
+  CONSTRAINT "ResourceAttributes_fkey" FOREIGN KEY ("serviceName", "resourceType", "resourceName")
+    REFERENCES "Resources" ("serviceName", "resourceType", "resourceName") MATCH FULL
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE "DefaultPermissionsForRoles" (
+  "serviceName" varchar(100) NOT NULL,
+  "resourceType" varchar(100) NOT NULL,
+  "resourceName" varchar(100) NOT NULL,
+  "attribute" varchar(250) NOT NULL,
+  "roleName" varchar(100) NOT NULL,
+  "permissions" smallint NOT NULL DEFAULT 0,
+  UNIQUE ("serviceName", "resourceType", "resourceName", "attribute", "roleName"),
+  CONSTRAINT "DefaultPermissionsForRoles_roleName_fkey" FOREIGN KEY ("roleName")
+    REFERENCES "Roles" ("roleName") MATCH SIMPLE
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+);

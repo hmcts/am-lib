43e8eea2e71eff03f166988d9a41436f1ef07957
Patryk Jar <jar.patryk@gmail.com>
2019-01-30 16:01:04 +0100

    AM-58: after code review

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/db/migration/V3__Create_default_roles_config_tables.sql
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/db/migration/V3__Create_default_roles_config_tables.sql
@@ -1,20 +1,20 @@
-CREATE TYPE securityclassification AS ENUM ('Public', 'Private', 'Restricted');
+CREATE TYPE SECURITYCLASSIFICATION AS ENUM ('Public', 'Private', 'Restricted');
 
 CREATE TABLE roles (
-  role_name varchar(100) PRIMARY KEY,
+  role_name VARCHAR(100) PRIMARY KEY,
   role_type VARCHAR (50) NOT NULL,
-  security_classification securityclassification NOT NULL
+  security_classification SECURITYCLASSIFICATION NOT NULL
 );
 
 CREATE TABLE services (
-  service_name varchar(100) PRIMARY KEY,
-  service_description varchar(250)
+  service_name VARCHAR(100) PRIMARY KEY,
+  service_description VARCHAR(250)
 );
 
 CREATE TABLE resources (
-  service_name varchar(100) NOT NULL,
-  resource_type varchar(100) NOT NULL,
-  resource_name varchar(100) NOT NULL,
+  service_name VARCHAR(100) NOT NULL,
+  resource_type VARCHAR(100) NOT NULL,
+  resource_name VARCHAR(100) NOT NULL,
   PRIMARY KEY (service_name, resource_type, resource_name),
   CONSTRAINT resources_service_name_fkey FOREIGN KEY (service_name)
     REFERENCES services (service_name)
@@ -22,26 +22,29 @@ CREATE TABLE resources (
 );
 
 CREATE TABLE resource_attributes (
-  service_name varchar(100) NOT NULL,
-  resource_type varchar(100) NOT NULL,
-  resource_name varchar(100) NOT NULL,
-  attribute varchar(250) NOT NULL,
-  default_security_classification securityclassification NOT NULL,
+  service_name VARCHAR(100) NOT NULL,
+  resource_type VARCHAR(100) NOT NULL,
+  resource_name VARCHAR(100) NOT NULL,
+  attribute VARCHAR(250) NOT NULL,
+  default_security_classification SECURITYCLASSIFICATION NOT NULL,
   PRIMARY KEY (service_name, resource_type, resource_name, attribute),
-  CONSTRAINT resource_attributes_fkey FOREIGN KEY (service_name, resource_type, resource_name)
+  CONSTRAINT resource_attributes_resources_fkey FOREIGN KEY (service_name, resource_type, resource_name)
     REFERENCES resources (service_name, resource_type, resource_name)
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE default_permissions_for_roles (
-  service_name varchar(100) NOT NULL,
-  resource_type varchar(100) NOT NULL,
-  resource_name varchar(100) NOT NULL,
-  attribute varchar(250) NOT NULL,
-  role_name varchar(100) NOT NULL,
-  permissions smallint NOT NULL DEFAULT 0,
+  service_name VARCHAR(100) NOT NULL,
+  resource_type VARCHAR(100) NOT NULL,
+  resource_name VARCHAR(100) NOT NULL,
+  attribute VARCHAR(250) NOT NULL,
+  role_name VARCHAR(100) NOT NULL,
+  permissions SMALLINT NOT NULL DEFAULT 0,
   UNIQUE (service_name, resource_type, resource_name, attribute, role_name),
   CONSTRAINT default_permissions_for_roles_roleName_fkey FOREIGN KEY (role_name)
     REFERENCES roles (role_name)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT default_permissions_for_roles_resource_attributes_fkey FOREIGN KEY (service_name, resource_type, resource_name, attribute)
+    REFERENCES resource_attributes (service_name, resource_type, resource_name, attribute)
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );

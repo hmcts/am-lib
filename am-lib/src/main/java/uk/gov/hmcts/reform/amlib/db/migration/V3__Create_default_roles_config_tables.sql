9e98dcaee8d19abc9d7398dad3934fc5a747f6f6
Patryk Jar <jar.patryk@gmail.com>
2019-01-30 12:23:21 +0100

    AM-58: underscore notation

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/db/migration/V3__Create_default_roles_config_tables.sql
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/db/migration/V3__Create_default_roles_config_tables.sql
@@ -1,47 +1,47 @@
-CREATE TYPE "SecurityClassification" AS ENUM ('Public', 'Private', 'Restricted');
+CREATE TYPE securityclassification AS ENUM ('Public', 'Private', 'Restricted');
 
-CREATE TABLE "Roles" (
-  "roleName" varchar(100) PRIMARY KEY,
-  "roleType" VARCHAR (50) NOT NULL,
-  "securityClassification" "SecurityClassification" NOT NULL
+CREATE TABLE roles (
+  role_name varchar(100) PRIMARY KEY,
+  role_type VARCHAR (50) NOT NULL,
+  security_classification securityclassification NOT NULL
 );
 
-CREATE TABLE "Services" (
-  "serviceName" varchar(100) PRIMARY KEY,
-  "serviceDescription" varchar(250)
+CREATE TABLE services (
+  service_name varchar(100) PRIMARY KEY,
+  service_description varchar(250)
 );
 
-CREATE TABLE "Resources" (
-  "serviceName" varchar(100) NOT NULL,
-  "resourceType" varchar(100) NOT NULL,
-  "resourceName" varchar(100) NOT NULL,
-  PRIMARY KEY ("serviceName", "resourceType", "resourceName"),
-  CONSTRAINT "Resources_serviceName_fkey" FOREIGN KEY ("serviceName")
-    REFERENCES "Services" ("serviceName") MATCH SIMPLE
+CREATE TABLE resources (
+  service_name varchar(100) NOT NULL,
+  resource_type varchar(100) NOT NULL,
+  resource_name varchar(100) NOT NULL,
+  PRIMARY KEY (service_name, resource_type, resource_name),
+  CONSTRAINT resources_service_name_fkey FOREIGN KEY (service_name)
+    REFERENCES services (service_name) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-CREATE TABLE "ResourceAttributes" (
-  "serviceName" varchar(100) NOT NULL,
-  "resourceType" varchar(100) NOT NULL,
-  "resourceName" varchar(100) NOT NULL,
-  "attribute" varchar(250) NOT NULL,
-  "defaultSecurityClassification" "SecurityClassification" NOT NULL,
-  PRIMARY KEY ("serviceName", "resourceType", "resourceName", "attribute"),
-  CONSTRAINT "ResourceAttributes_fkey" FOREIGN KEY ("serviceName", "resourceType", "resourceName")
-    REFERENCES "Resources" ("serviceName", "resourceType", "resourceName") MATCH FULL
+CREATE TABLE resource_attributes (
+  service_name varchar(100) NOT NULL,
+  resource_type varchar(100) NOT NULL,
+  resource_name varchar(100) NOT NULL,
+  attribute varchar(250) NOT NULL,
+  default_security_classification securityclassification NOT NULL,
+  PRIMARY KEY (service_name, resource_type, resource_name, attribute),
+  CONSTRAINT resource_attributes_fkey FOREIGN KEY (service_name, resource_type, resource_name)
+    REFERENCES resources (service_name, resource_type, resource_name) MATCH FULL
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-CREATE TABLE "DefaultPermissionsForRoles" (
-  "serviceName" varchar(100) NOT NULL,
-  "resourceType" varchar(100) NOT NULL,
-  "resourceName" varchar(100) NOT NULL,
-  "attribute" varchar(250) NOT NULL,
-  "roleName" varchar(100) NOT NULL,
-  "permissions" smallint NOT NULL DEFAULT 0,
-  UNIQUE ("serviceName", "resourceType", "resourceName", "attribute", "roleName"),
-  CONSTRAINT "DefaultPermissionsForRoles_roleName_fkey" FOREIGN KEY ("roleName")
-    REFERENCES "Roles" ("roleName") MATCH SIMPLE
+CREATE TABLE default_permissions_for_roles (
+  service_name varchar(100) NOT NULL,
+  resource_type varchar(100) NOT NULL,
+  resource_name varchar(100) NOT NULL,
+  attribute varchar(250) NOT NULL,
+  role_name varchar(100) NOT NULL,
+  permissions smallint NOT NULL DEFAULT 0,
+  UNIQUE (service_name, resource_type, resource_name, attribute, role_name),
+  CONSTRAINT default_permissions_for_roles_roleName_fkey FOREIGN KEY (role_name)
+    REFERENCES roles (role_name) MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION
 );

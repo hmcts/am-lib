779fd515bd7b47cfb8dc9bd95e9068b8e7151c49
Kamil Zielinski <K.Zielinski@kainos.com>
2019-01-30 10:08:58 +0100

    Added tests

--- am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/AccessManagementServiceIntegrationTest.java
+++ am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/AccessManagementServiceIntegrationTest.java
@@ -1,5 +1,8 @@
 package integration.uk.gov.hmcts.reform.amlib;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.UUID;
@@ -9,11 +12,15 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class AccessManagementServiceIntegrationTest extends IntegrationBaseTest {
 
     private String resourceId;
+    private JsonNode jsonObject = JsonNodeFactory.instance.objectNode();
+
+    @Before
+    public void testSetup() {
+        resourceId = UUID.randomUUID().toString();
+    }
 
     @Test
-    public void whenCreatingResourceAccess_ResourceAccessAppearsInDatabase() {
-        resourceId = UUID.randomUUID().toString();
-
+    public void createQuery_whenCreatingResourceAccess_ResourceAccessAppearsInDatabase() {
         ams.createResourceAccess(resourceId, "dsa");
 
         int count = jdbi.open().createQuery(
@@ -23,5 +30,24 @@ public class AccessManagementServiceIntegrationTest extends IntegrationBaseTest 
                 .findOnly();
 
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void filterResource_whenRowExistWithAccessorIdAndResourceId_ReturnPassedJsonObject() {
+        String accessorId = UUID.randomUUID().toString();
+        ams.createResourceAccess(resourceId, accessorId);
+
+        JsonNode result = ams.filterResource(accessorId, resourceId, jsonObject);
+
+        assertThat(result).isEqualTo(jsonObject);
+    }
+
+    @Test
+    public void filterResource_whenRowNotExistWithAccessorIdAndResourceId_ReturnNull() {
+        ams.createResourceAccess("abc", "def");
+
+        JsonNode result = ams.filterResource("ijk", "lmn", jsonObject);
+
+        assertThat(result).isEqualTo(null);
     }
 }

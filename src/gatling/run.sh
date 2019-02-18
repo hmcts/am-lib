41294a4b9dd59068c781ca536582c621aa3133ec
Damian Dunajski <damian.dunajski@hmcts.net>
2019-02-18 15:13:52 +0000

    Rename reports dir

--- src/gatling/run.sh
+++ src/gatling/run.sh
@@ -2,7 +2,7 @@
 
 dir=$(realpath $(dirname ${0}))
 
-command='gatling.sh -m -sf /gatling/workspace/simulations -bdf /gatling/workspace/bodies -rf /gatling/workspace/reports'
+command='gatling.sh -m -sf /gatling/workspace/simulations -bdf /gatling/workspace/bodies -rf /gatling/workspace/results'
 
 docker run -it --network=host \
     -v ${dir}/conf:/etc/gatling/conf \

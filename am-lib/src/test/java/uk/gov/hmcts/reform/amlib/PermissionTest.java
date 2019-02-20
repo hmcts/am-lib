df1e49ab2df7b1dc66ce3272cad405ef9a8f549d
Damian Dunajski <damian.dunajski@hmcts.net>
2019-02-20 12:21:09 +0000

    Rewrite more permissions tests using junit 5 goodies

--- am-lib/src/test/java/uk/gov/hmcts/reform/amlib/PermissionTest.java
+++ am-lib/src/test/java/uk/gov/hmcts/reform/amlib/PermissionTest.java
@@ -2,14 +2,12 @@ package uk.gov.hmcts.reform.amlib;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.amlib.enums.Permission;
 import uk.gov.hmcts.reform.amlib.exceptions.UnsupportedPermissionsException;
 import uk.gov.hmcts.reform.amlib.utils.Permissions;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -24,75 +22,68 @@ import static uk.gov.hmcts.reform.amlib.enums.Permission.UPDATE;
 @SuppressWarnings("PMD")
 class PermissionTest {
 
-    @Test
-    void sumOf_whenPassingSetOfPermissions_theSumOfValuesIsCalculated() {
-        Set<Permission> permissions = Stream.of(CREATE, Permission.READ).collect(Collectors.toSet());
-
-        int sum = Permissions.sumOf(permissions);
-        int expectedSum = CREATE.getValue() + Permission.READ.getValue();
-
-        assertThat(sum).isEqualTo(expectedSum);
-    }
-
-    @Test
-    void sumOf_whenPassingAPermission_theSumOfValuesIsCalculated() {
-        int sum = Permissions.sumOf(CREATE);
-        int expectedSum = CREATE.getValue();
-
-        assertThat(sum).isEqualTo(expectedSum);
-    }
-
-    @Test
-    void sumOf_whenPassingNoPermissions_theSumOfValuesIsZero() {
-        Set<Permission> permissions = new HashSet<>();
-
-        int sum = Permissions.sumOf(permissions);
-
-        assertThat(sum).isEqualTo(0);
+    @ParameterizedTest
+    @MethodSource("createArguments")
+    void sumOf_shouldCalculateSumOfPermissionsFromArray(Arguments args) {
+        int sumOfPermissions = Permissions.sumOf(args.permissions);
+        assertThat(sumOfPermissions).isEqualTo(args.sumOfPermissions);
     }
 
     @ParameterizedTest
-    @MethodSource("createPermissionTestData")
-    void fromSumOf_shouldMapIntegerValueToSetOfPermissions(int sumOfPermissions, Set<Permission> permissions) {
-        assertThat(Permissions.fromSumOf(sumOfPermissions))
-            .containsOnlyElementsOf(permissions);
+    @MethodSource("createArguments")
+    void sumOf_shouldCalculateSumOfPermissionsFromSet(Arguments args) {
+        int sumOfPermissions = Permissions.sumOf(Arrays.stream(args.permissions).collect(Collectors.toSet()));
+        assertThat(sumOfPermissions).isEqualTo(args.sumOfPermissions);
     }
 
-    private static Stream<Arguments> createPermissionTestData() {
-        return Stream.of(
-            Arguments.of(1, setOf(CREATE)),
-            Arguments.of(2, setOf(READ)),
-            Arguments.of(3, setOf(CREATE, READ)),
-            Arguments.of(4, setOf(UPDATE)),
-            Arguments.of(5, setOf(CREATE, UPDATE)),
-            Arguments.of(6, setOf(READ, UPDATE)),
-            Arguments.of(7, setOf(CREATE, READ, UPDATE)),
-            Arguments.of(8, setOf(DELETE)),
-            Arguments.of(9, setOf(CREATE, DELETE)),
-            Arguments.of(10, setOf(READ, DELETE)),
-            Arguments.of(11, setOf(CREATE, READ, DELETE)),
-            Arguments.of(12, setOf(UPDATE, DELETE)),
-            Arguments.of(13, setOf(CREATE, UPDATE, DELETE)),
-            Arguments.of(14, setOf(READ, UPDATE, DELETE)),
-            Arguments.of(15, setOf(CREATE, READ, UPDATE, DELETE))
-        );
+    @ParameterizedTest
+    @MethodSource("createArguments")
+    void fromSumOf_shouldDeductSetOfPermissionsFromNumericSum(Arguments args) {
+        Set<Permission> permissions = Permissions.fromSumOf(args.sumOfPermissions);
+        assertThat(permissions).containsOnly(args.permissions);
     }
 
     @Test
-    void fromSumOf_sumOfPermissionsValueHighValue_ShouldThrowUnsupportedPermission() {
+    void fromSumOf_shouldThrowExceptionWhenPermissionSumIsGreaterThenMaxValue() {
         assertThatExceptionOfType(UnsupportedPermissionsException.class).isThrownBy(
             () -> Permissions.fromSumOf(Permissions.MAX_PERMISSIONS_VALUE + 1))
             .withMessage("The given permissions are not supported");
     }
 
     @Test
-    void fromSumOf_sumOfPermissionsNegativeValue_ExpectArray() {
+    void fromSumOf_shouldThrowExceptionWhenPermissionSumIsLesserThenMinValue() {
         assertThatExceptionOfType(UnsupportedPermissionsException.class).isThrownBy(
             () -> Permissions.fromSumOf(Permissions.MIN_PERMISSIONS_VALUE - 5))
             .withMessage("The given permissions are not supported");
     }
 
-    private static Set<Permission> setOf(Permission... permissions) {
-        return Arrays.stream(permissions).collect(Collectors.toSet());
+    private static Stream<Arguments> createArguments() {
+        return Stream.of(
+            new Arguments(1, CREATE),
+            new Arguments(2, READ),
+            new Arguments(3, CREATE, READ),
+            new Arguments(4, UPDATE),
+            new Arguments(5, CREATE, UPDATE),
+            new Arguments(6, READ, UPDATE),
+            new Arguments(7, CREATE, READ, UPDATE),
+            new Arguments(8, DELETE),
+            new Arguments(9, CREATE, DELETE),
+            new Arguments(10, READ, DELETE),
+            new Arguments(11, CREATE, READ, DELETE),
+            new Arguments(12, UPDATE, DELETE),
+            new Arguments(13, CREATE, UPDATE, DELETE),
+            new Arguments(14, READ, UPDATE, DELETE),
+            new Arguments(15, CREATE, READ, UPDATE, DELETE)
+        );
+    }
+
+    private static class Arguments {
+        private final int sumOfPermissions;
+        private final Permission[] permissions;
+
+        private Arguments(int sumOfPermissions, Permission... permissions) {
+            this.sumOfPermissions = sumOfPermissions;
+            this.permissions = permissions;
+        }
     }
 }

da37e738f1ec6dca1004a12e6139ccb1819b265c
Rebecca Brennan <rebeccabrennan@GROMMASH.local>
2019-03-05 12:17:11 +0000

    refactored java docs to kick off build(Am-135)

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/AccessManagementService.java
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/AccessManagementService.java
@@ -35,7 +35,7 @@ public class AccessManagementService {
     /**
      * Grants explicit access to resource accordingly to record configuration.
      *
-     * <p>Operation is performed in transaction so that if not all records can be created then whole grant will fail.
+     * <p>Operation is performed in a transaction so that if not all records can be created then whole grant will fail.
      *
      * @param explicitAccessGrant an object that describes explicit access to resource
      */

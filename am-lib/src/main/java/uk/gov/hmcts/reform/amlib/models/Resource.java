be3607bf24d6f7b43dcdba4918421ec17f6726b6
swalker125 <sam.walker@digit-hull.com>
2019-03-07 10:47:22 +0000

    removed unused imports

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/models/Resource.java
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/models/Resource.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.amlib.models;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
--- src/main/java/uk/gov/hmcts/reform/amapi/models/FilterResource.java
+++ src/main/java/uk/gov/hmcts/reform/amapi/models/FilterResource.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.amapi.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.amlib.models.Resource;
@@ -9,7 +8,6 @@ import java.util.Set;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class FilterResource {
     private final String userId;
     private final Set<String> userRoles;

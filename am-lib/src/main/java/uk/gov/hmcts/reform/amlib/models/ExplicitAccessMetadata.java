839c887e3d61093453ff31b339abed4990b4ebf5
Rebecca Brennan <rebeccabrennan@GROMMASH.local>
2019-03-07 14:35:16 +0000

    String builder added to metadata to append % to attribute

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/models/ExplicitAccessMetadata.java
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/models/ExplicitAccessMetadata.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @SuppressWarnings("PMD")
 public final class ExplicitAccessMetadata extends AbstractAccessMetadata {
+    StringBuilder stringBuilder = new StringBuilder();
 
     @Builder // All args constructor is needs for builder. @SuperBuilder cannot be used because IDE does not support it
     @SuppressWarnings("squid:S00107") // Having so many arguments seems reasonable solution here
@@ -19,7 +20,7 @@ public final class ExplicitAccessMetadata extends AbstractAccessMetadata {
                                    String resourceName,
                                    String attribute,
                                    String securityClassification) {
-        super(resourceId, accessorId, accessType, serviceName, resourceType, resourceName, attribute + "%",
+        super(resourceId, accessorId, accessType, serviceName, resourceType, resourceName, attribute + new StringBuilder().append("%"),
             securityClassification);
     }
 }

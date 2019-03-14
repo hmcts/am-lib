7e44ffcf618f252ca4bcde2b6b8db4f922d9c203
swalker125 <sam.walker@digit-hull.com>
2019-03-14 17:21:22 +0000

    feat: added NONE value to security classification enum (AM-231)
    proposed format for storing states and events is /__state/ and /__event/

--- am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/GrantAccessIntegrationTest.java
+++ am-lib/src/test/java/integration/uk/gov/hmcts/reform/amlib/GrantAccessIntegrationTest.java
@@ -2,10 +2,13 @@ package integration.uk.gov.hmcts.reform.amlib;
 
 import com.fasterxml.jackson.core.JsonPointer;
 import integration.uk.gov.hmcts.reform.amlib.base.PreconfiguredIntegrationBaseTest;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.amlib.AccessManagementService;
 import uk.gov.hmcts.reform.amlib.enums.Permission;
+import uk.gov.hmcts.reform.amlib.enums.SecurityClassification;
+import uk.gov.hmcts.reform.amlib.models.ExplicitAccessGrant;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -16,10 +19,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.ACCESSOR_ID;
+import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.ACCESS_TYPE;
 import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.EXPLICIT_READ_CREATE_UPDATE_PERMISSIONS;
 import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.READ_PERMISSION;
+import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.RESOURCE_NAME;
+import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.RESOURCE_TYPE;
+import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.SERVICE_NAME;
 import static uk.gov.hmcts.reform.amlib.helpers.TestDataFactory.createGrant;
 import static uk.gov.hmcts.reform.amlib.helpers.TestDataFactory.createGrantForWholeDocument;
+import static uk.gov.hmcts.reform.amlib.helpers.TestDataFactory.createPermissions;
 import static uk.gov.hmcts.reform.amlib.helpers.TestDataFactory.createPermissionsForWholeDocument;
 
 class GrantAccessIntegrationTest extends PreconfiguredIntegrationBaseTest {
@@ -72,6 +80,38 @@ class GrantAccessIntegrationTest extends PreconfiguredIntegrationBaseTest {
     void whenCreatingDuplicateResourceAccessEntryIsOverwritten() {
         service.grantExplicitResourceAccess(createGrantForWholeDocument(resourceId, READ_PERMISSION));
         service.grantExplicitResourceAccess(createGrantForWholeDocument(resourceId, READ_PERMISSION));
+
+        assertThat(databaseHelper.countExplicitPermissions(resourceId)).isEqualTo(1);
+    }
+
+    @Test
+    void whenStoringStateShouldAllowEntryWithSecurityClassificationEqualNone() {
+        service.grantExplicitResourceAccess(ExplicitAccessGrant.builder()
+            .accessorId(ACCESSOR_ID)
+            .accessType(ACCESS_TYPE)
+            .resourceId(resourceId)
+            .resourceName(RESOURCE_NAME)
+            .resourceType(RESOURCE_TYPE)
+            .serviceName(SERVICE_NAME)
+            .attributePermissions(createPermissions("/__state/closed", READ_PERMISSION))
+            .securityClassification(SecurityClassification.NONE)
+            .build());
+
+        assertThat(databaseHelper.countExplicitPermissions(resourceId)).isEqualTo(1);
+    }
+
+    @Test
+    void whenStoringEventShouldAllowEntryWithSecurityClassificationEqualNone() {
+        service.grantExplicitResourceAccess(ExplicitAccessGrant.builder()
+            .accessorId(ACCESSOR_ID)
+            .accessType(ACCESS_TYPE)
+            .resourceId(resourceId)
+            .resourceName(RESOURCE_NAME)
+            .resourceType(RESOURCE_TYPE)
+            .serviceName(SERVICE_NAME)
+            .attributePermissions(createPermissions("/__event/enterPlea", READ_PERMISSION))
+            .securityClassification(SecurityClassification.NONE)
+            .build());
 
         assertThat(databaseHelper.countExplicitPermissions(resourceId)).isEqualTo(1);
     }

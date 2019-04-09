c71fb737ef5e535f33f107ffe06aff0ae69ed03b
Damian Dunajski <damdun@gmail.com>
2019-04-09 12:41:14 +0100

    Refactor code

--- am-lib/src/benchmark/java/uk/gov/hmcts/reform/amlib/AccessManagementServiceBenchmarks.java
+++ am-lib/src/benchmark/java/uk/gov/hmcts/reform/amlib/AccessManagementServiceBenchmarks.java
@@ -1,120 +1,18 @@
 package uk.gov.hmcts.reform.amlib;
 
-import com.fasterxml.jackson.core.JsonPointer;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.postgresql.ds.PGPoolingDataSource;
-import uk.gov.hmcts.reform.amlib.config.DatabaseProperties;
-import uk.gov.hmcts.reform.amlib.enums.Permission;
-import uk.gov.hmcts.reform.amlib.enums.SecurityClassification;
-import uk.gov.hmcts.reform.amlib.models.ExplicitAccessGrant;
-import uk.gov.hmcts.reform.amlib.models.Resource;
-import uk.gov.hmcts.reform.amlib.models.ResourceDefinition;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
-
-import static java.lang.System.getenv;
-import static uk.gov.hmcts.reform.amlib.enums.Permission.CREATE;
-import static uk.gov.hmcts.reform.amlib.enums.Permission.DELETE;
-import static uk.gov.hmcts.reform.amlib.enums.Permission.READ;
-import static uk.gov.hmcts.reform.amlib.enums.Permission.UPDATE;
+import uk.gov.hmcts.reform.amlib.states.BenchmarkState;
+import uk.gov.hmcts.reform.amlib.states.DataState;
 
 @BenchmarkMode(Mode.Throughput)
 @SuppressWarnings({"PMD.NonStaticInitializer", "PMD.EmptyCatchBlock"})
 public class AccessManagementServiceBenchmarks {
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-
-    @State(Scope.Thread)
-    @SuppressWarnings("LineLength")
-    public static class RecordState {
-        String resourceId = UUID.randomUUID().toString();
-        String accessorId = UUID.randomUUID().toString();
-
-        //load in mock FPL case data
-        JsonNode inputJson;
-
-        {
-            try {
-                inputJson = MAPPER.readTree(ClassLoader.getSystemResource("data.json"));
-            } catch (IOException e) {
-                //NO-OP
-            }
-        }
-
-
-        PGPoolingDataSource dataSource = new PGPoolingDataSource();
-        {
-            DatabaseProperties databaseProperties = DatabaseProperties.createFromEnvironmentProperties();
-            dataSource.setServerName(databaseProperties.getServer().getHost());
-            dataSource.setPortNumber(databaseProperties.getServer().getPort());
-            dataSource.setDatabaseName(databaseProperties.getDatabase());
-            dataSource.setUser(databaseProperties.getCredentials().getUsername());
-            dataSource.setPassword(databaseProperties.getCredentials().getPassword());
-            dataSource.setMaxConnections(64);
-        }
-
-        //initialise service with datasource. Uses springboot DataSourceBuilder.
-        AccessManagementService service = new AccessManagementService(dataSource);
-
-        //need to define role constant and add to AAT (for role based)
-
-        //define resource constant
-        Resource resource = Resource.builder()
-            .id(resourceId)
-            .definition(ResourceDefinition.builder()
-                .serviceName("Service 1")
-                .resourceType("Resource Type 1")
-                .resourceName("resource")
-                .build())
-            .data(inputJson)
-            .build();
-
-        @Setup
-        public void doSetup() {
-            //create map of explicit access JsonPointer -> Permission
-            Map<JsonPointer, Set<Permission>> attributePermissions =
-                ImmutableMap.<JsonPointer, Set<Permission>>builder()
-                    .put(JsonPointer.valueOf(""), ImmutableSet.of(READ, CREATE))
-                    .put(JsonPointer.valueOf("/orders"), ImmutableSet.of(CREATE))
-                    .put(JsonPointer.valueOf("/orders/orderType"), ImmutableSet.of(READ))
-                    .put(JsonPointer.valueOf("/orders/emergencyProtectionOrderDirections"), ImmutableSet.of(DELETE))
-                    .put(JsonPointer.valueOf("/caseName"), ImmutableSet.of(UPDATE, CREATE, READ, DELETE))
-                    .put(JsonPointer.valueOf("/children"), ImmutableSet.of(READ))
-                    .put(JsonPointer.valueOf("/children/firstChild"), ImmutableSet.of(UPDATE))
-                    .put(JsonPointer.valueOf("/children/adoption"), ImmutableSet.of(READ, CREATE))
-                    .put(JsonPointer.valueOf("/applicant"), ImmutableSet.of(READ, CREATE))
-                    .put(JsonPointer.valueOf("/solicitor"), ImmutableSet.of(UPDATE, DELETE, CREATE))
-                    .build();
-
-            //create explicit access grant
-            service.grantExplicitResourceAccess(ExplicitAccessGrant.builder()
-                .resourceId(resourceId)
-                .accessorIds(ImmutableSet.of(accessorId))
-                .accessType("EXPLICIT")
-                .serviceName("Service 1")
-                .resourceType("Resource Type 1")
-                .resourceName("resource")
-                .attributePermissions(attributePermissions)
-                .securityClassification(SecurityClassification.PUBLIC)
-                .build()
-            );
-        }
-    }
-
     @Benchmark
-    public void filterResourceBenchmark(RecordState state) {
-        state.service.filterResource(state.accessorId, ImmutableSet.of("DOES_NOT_EXIST"), state.resource);
+    public void filterResourceBenchmark(BenchmarkState benchmark, DataState data) {
+        benchmark.service.filterResource(data.accessorId, ImmutableSet.of("DOES_NOT_EXIST"), data.resource);
     }
 }
---
+++ am-lib/src/benchmark/java/uk/gov/hmcts/reform/amlib/states/BenchmarkState.java
@@ -0,0 +1,72 @@
+package uk.gov.hmcts.reform.amlib.states;
+
+import com.fasterxml.jackson.core.JsonPointer;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.postgresql.ds.PGPoolingDataSource;
+import uk.gov.hmcts.reform.amlib.AccessManagementService;
+import uk.gov.hmcts.reform.amlib.config.DatabaseProperties;
+import uk.gov.hmcts.reform.amlib.enums.Permission;
+import uk.gov.hmcts.reform.amlib.enums.SecurityClassification;
+import uk.gov.hmcts.reform.amlib.models.ExplicitAccessGrant;
+
+import javax.sql.DataSource;
+import java.util.Map;
+import java.util.Set;
+
+import static uk.gov.hmcts.reform.amlib.enums.Permission.*;
+
+@State(Scope.Benchmark)
+public class BenchmarkState {
+    public AccessManagementService service;
+
+    @Setup
+    public void populateDatabase(DataState state) {
+        Map<JsonPointer, Set<Permission>> attributePermissions =
+            ImmutableMap.<JsonPointer, Set<Permission>>builder()
+                .put(JsonPointer.valueOf(""), ImmutableSet.of(READ, CREATE))
+                .put(JsonPointer.valueOf("/orders"), ImmutableSet.of(CREATE))
+                .put(JsonPointer.valueOf("/orders/orderType"), ImmutableSet.of(READ))
+                .put(JsonPointer.valueOf("/orders/emergencyProtectionOrderDirections"), ImmutableSet.of(DELETE))
+                .put(JsonPointer.valueOf("/caseName"), ImmutableSet.of(UPDATE, CREATE, READ, DELETE))
+                .put(JsonPointer.valueOf("/children"), ImmutableSet.of(READ))
+                .put(JsonPointer.valueOf("/children/firstChild"), ImmutableSet.of(UPDATE))
+                .put(JsonPointer.valueOf("/children/adoption"), ImmutableSet.of(READ, CREATE))
+                .put(JsonPointer.valueOf("/applicant"), ImmutableSet.of(READ, CREATE))
+                .put(JsonPointer.valueOf("/solicitor"), ImmutableSet.of(UPDATE, DELETE, CREATE))
+                .build();
+
+        service.grantExplicitResourceAccess(ExplicitAccessGrant.builder()
+            .resourceId(state.resource.getId())
+            .accessorIds(ImmutableSet.of(state.accessorId))
+            .accessType("EXPLICIT")
+            .serviceName("Service 1")
+            .resourceType("Resource Type 1")
+            .resourceName("resource")
+            .attributePermissions(attributePermissions)
+            .securityClassification(SecurityClassification.PUBLIC)
+            .build()
+        );
+    }
+
+    @Setup
+    public void initiateService() {
+        service = new AccessManagementService(createDataSource());
+    }
+
+    @SuppressWarnings({"deprecation"})
+    private DataSource createDataSource() {
+        PGPoolingDataSource dataSource = new PGPoolingDataSource();
+        DatabaseProperties databaseProperties = DatabaseProperties.createFromEnvironmentProperties();
+        dataSource.setServerName(databaseProperties.getServer().getHost());
+        dataSource.setPortNumber(databaseProperties.getServer().getPort());
+        dataSource.setDatabaseName(databaseProperties.getDatabase());
+        dataSource.setUser(databaseProperties.getCredentials().getUsername());
+        dataSource.setPassword(databaseProperties.getCredentials().getPassword());
+        dataSource.setMaxConnections(64);
+        return dataSource;
+    }
+}

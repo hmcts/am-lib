281e1b25be4004806ea151461ac536e9234f7557
Ben Matthews <benjamin.matthews@hmcts.net>
2019-05-30 12:27:52 +0100

    refactor: db info prompt

--- migration/ccd-to-am-migration-runner.sh
+++ migration/ccd-to-am-migration-runner.sh
@@ -4,14 +4,22 @@ echo
 set -e
 set -u
 
-DB="am"
-USER="amuser"
 INIT_SQL="./ccd-to-am-migration-init.sql"
 MAIN_SQL="./ccd-to-am-migration-main.sql"
+
+echo "* CCD to AM migration..."
+echo
+read -p "* AM DB hostname: " HOST
+read -p "* AM DB port: " PORT
+read -p "* AM DB name: " DB
+read -p "* AM DB username: " USER
+echo
 
 psql \
     -X \
     -q \
+    -h $HOST \
+    -p $PORT \
     -U $USER \
     -f $INIT_SQL \
     --set AUTOCOMMIT=off \
@@ -21,11 +29,14 @@ psql \
 psql \
     -X \
     -q \
+    -h $HOST \
+    -p $PORT \
     -U $USER \
     -f $MAIN_SQL \
     --set AUTOCOMMIT=off \
     --set ON_ERROR_STOP=off \
     $DB
 
-echo "migration successful"
+echo "* Migration finished"
+echo
 exit 0

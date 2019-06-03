16cdc5318b6b23f60fc99e07d152d85590d932e1
Ben Matthews <benjamin.matthews@hmcts.net>
2019-06-03 14:34:14 +0100

    feat: migration error handling

--- migration/ccd-to-am-migration-main.sql
+++ migration/ccd-to-am-migration-main.sql
@@ -5,8 +5,7 @@ CREATE TEMP TABLE stage
 (
     case_data_id VARCHAR, case_type_id VARCHAR,
     user_id VARCHAR, case_role VARCHAR
-)
-ON COMMIT DROP;
+);
 
 ALTER TABLE access_management DROP CONSTRAINT access_management_resources_fkey;
 ALTER TABLE access_management DROP CONSTRAINT relationship_fkey;
@@ -15,30 +14,25 @@ ALTER TABLE access_management DROP CONSTRAINT relationship_fkey;
 
 SELECT COUNT(*) AS "columns to migrate" FROM stage;
 
-WITH ins_access_management AS
-(
-    INSERT INTO access_management (resource_id, accessor_type, accessor_id,
-            "attribute", permissions, service_name, resource_name,
-            resource_type, relationship)
-        SELECT s.case_data_id AS resource_id, 'USER' AS accessor_type,
-            s.user_id AS accessor_id, ra."attribute" AS "attribute",
-            dp.permissions AS permissions, re.service_name AS service_name,
-            s.case_type_id AS resource_name, re.resource_type AS resource_type,
-            s.case_role AS relationship
-        FROM stage AS s, resource_attributes AS ra,
-            default_permissions_for_roles AS dp, resources AS re
-        WHERE ra.resource_name = s.case_type_id
-        AND dp.resource_name = s.case_type_id
-        AND re.resource_name = s.case_type_id
-        AND s.case_role IN (SELECT role_name FROM roles)
-    EXCEPT
-        SELECT resource_id, accessor_type, accessor_id, "attribute",
-            permissions, service_name, resource_name, resource_type,
-            relationship
-        FROM access_management
-    RETURNING resource_id
-)
-SELECT COUNT(*) AS "access_management inserts" FROM ins_access_management;
+INSERT INTO access_management (resource_id, accessor_type, accessor_id,
+        "attribute", permissions, service_name, resource_name,
+        resource_type, relationship)
+    SELECT s.case_data_id AS resource_id, 'USER' AS accessor_type,
+        s.user_id AS accessor_id, ra."attribute" AS "attribute",
+        dp.permissions AS permissions, re.service_name AS service_name,
+        s.case_type_id AS resource_name, re.resource_type AS resource_type,
+        s.case_role AS relationship
+    FROM stage AS s, resource_attributes AS ra,
+        default_permissions_for_roles AS dp, resources AS re
+    WHERE ra.resource_name = s.case_type_id
+    AND dp.resource_name = s.case_type_id
+    AND re.resource_name = s.case_type_id
+    AND s.case_role IN (SELECT role_name FROM roles)
+EXCEPT
+    SELECT resource_id, accessor_type, accessor_id, "attribute",
+        permissions, service_name, resource_name, resource_type,
+        relationship
+    FROM access_management;
 
 ALTER TABLE access_management ADD CONSTRAINT access_management_resources_fkey
     FOREIGN KEY (service_name, resource_type, resource_name)
@@ -46,6 +40,8 @@ ALTER TABLE access_management ADD CONSTRAINT access_management_resources_fkey
 ALTER TABLE access_management ADD CONSTRAINT relationship_fkey
     FOREIGN KEY (relationship)
     REFERENCES roles(role_name);
+
+COMMIT;
 
 WITH errors AS
 (
@@ -61,6 +57,7 @@ SELECT * FROM stage
 EXCEPT
     SELECT resource_id AS case_data_id, resource_name AS case_type_id,
         accessor_id AS user_id, relationship AS case_role
-    FROM access_management;
+    FROM access_management
+    LIMIT 100;
 
 COMMIT;

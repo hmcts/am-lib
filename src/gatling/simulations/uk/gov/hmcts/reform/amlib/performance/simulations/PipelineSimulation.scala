737cb553b6dde86484d49e70ece488d619fb6255
shashank-rastogi <shashank.rastogi@hmcts.net>
2019-06-14 12:42:22 +0100

    enhancing the test to run with concurrent load of 100 users per sec for 10 mins

--- src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/simulations/PipelineSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/simulations/PipelineSimulation.scala
@@ -10,17 +10,37 @@ import scala.concurrent.duration._
 class PipelineSimulation extends Simulation {
 
   private val httpProtocol = http.baseUrl(Environment.baseUrl)
-  private val loadProfile = rampUsers(10) during 2.seconds
+  private val loadProfile = rampUsers(50) during 20.seconds
 
   /* load profile and assertions to be changed once NFRs are defined
       this is just an exemplary simulation */
 
-  setUp(
+  /*setUp(
     BasicScenarios.createResourceAccess.inject(loadProfile).protocols(httpProtocol),
     BasicScenarios.filterResource.inject(loadProfile).protocols(httpProtocol),
     BasicScenarios.revokeResourceAccess.inject(loadProfile).protocols(httpProtocol)
-  ).maxDuration(30.seconds)
+  ).maxDuration(600.seconds)
     .assertions(
+      global.failedRequests.count.is(0),
+      global.responseTime.max.lt(30000)
+    )
+*/
+
+  setUp(
+    BasicScenarios.createResourceAccess.inject(
+        constantUsersPerSec(100) during (10 minutes)).protocols(httpProtocol),
+    BasicScenarios.filterResource.inject(
+        constantUsersPerSec(100) during (10 minutes)).protocols(httpProtocol),
+    BasicScenarios.revokeResourceAccess.inject(
+        constantUsersPerSec(100) during (10 minutes)).protocols(httpProtocol)
+  )
+  .throttle(
+    reachRps(100) in (10 seconds),
+    holdFor(1 minute),
+    jumpToRps(50),
+    holdFor(5 minutes)
+  )
+  .assertions(
       global.failedRequests.count.is(0),
       global.responseTime.max.lt(30000)
     )

faea9d4b0abea17f71b0f38281168100ba61e031
Siva Kanukollu <siva.kanukollu@hmcts.net>
2019-07-16 13:45:16 +0100

    AM-338: Serenity framework setup.

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/HealthCheckTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/HealthCheckTest.java
@@ -1,18 +1,23 @@
 package uk.gov.hmcts.reform.amapi.functional;
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Test;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
-import static io.restassured.RestAssured.get;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-class HealthCheckTest extends RestAssuredTest {
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert"})
+public class HealthCheckTest extends FunctionalTestSuite {
 
     @Test
     @Tag("SmokeTest")
-    void healthcheck_returns_200() {
-        get("/health")
+    public void healthcheckReturns200() {
+        amApiClient.buildRequest().get("http://localhost:3704/health")
             .then().statusCode(200)
             .and().body("status", equalTo("UP"));
     }
+
 }

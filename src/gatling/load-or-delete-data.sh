3a05e84bebb010896540c72103171b9327a09b70
abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
2019-07-16 16:36:32 +0100

    Fix:Gating test Initial data load Jenkins(AM-362) (#248)
    * fix: gatling fix initial data load Jenkins aat(Am-362)
    
    * fix: vulnerability for tomcat CVE-2019-10072
    
    * fix:Gtaing test Initial data load Jenkins(AM-362)

--- src/gatling/load-or-delete-data.sh
+++ src/gatling/load-or-delete-data.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "start data load"
 DATABASE_USER=$3;
 DATABASE_NAME=$4;
 
@@ -16,9 +17,12 @@ else
     FILE_NAME="delete-data"
 fi
 if [ "$8" = "aat" ]; then
+  echo "executing for aat and file name $FILE_NAME.sql"
   export PGPASSWORD=$6
   psql -h $5 -d $DATABASE_NAME -U $DATABASE_USER -p $7 -f $(dirname $0)/testdata/$FILE_NAME.sql
 else
-docker cp $1 am-lib-testing-service-db:/tmp/
-docker exec -i am-lib-testing-service-db psql -U $DATABASE_USER -d $DATABASE_NAME -f /tmp/testdata/$FILE_NAME.sql
+  echo "executing for local"
+  docker cp $1 am-lib-testing-service-db:/tmp/
+  docker exec -i am-lib-testing-service-db psql -U $DATABASE_USER -d $DATABASE_NAME -f /tmp/testdata/$FILE_NAME.sql
 fi
+echo "completed data load"

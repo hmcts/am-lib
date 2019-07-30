f487de57e6f3d45903d6b95c37d5ac63defc2342
Ben Matthews <benjamin.matthews@hmcts.net>
2019-07-30 10:00:25 +0100

    refactor: mkdir change and comment out other tests

--- security.sh
+++ security.sh
@@ -3,6 +3,6 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001 -l FAIL
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-mkdir functional-output
+mkdir -p functional-output
 cp /zap/api-report.html functional-output/
 zap-cli -p 1001 alerts -l High

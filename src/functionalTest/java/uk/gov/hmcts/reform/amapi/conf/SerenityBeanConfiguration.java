aeb62193b447ef620c1040ee3fc6915735ace905
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-07 21:00:00 +0100

    test:Am-338 load/delete function test data serenity

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/conf/SerenityBeanConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/conf/SerenityBeanConfiguration.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.amapi.conf;
 
-import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.amlib.DefaultRoleSetupImportService;
-
-import javax.sql.DataSource;
 
 import static java.lang.System.getenv;
 
@@ -15,20 +12,15 @@ public class SerenityBeanConfiguration {
 
     @Bean
     DefaultRoleSetupImportService getDefaultRoleSetupImportService() {
-        return new DefaultRoleSetupImportService(createDataSource());
+
+        String username = getValueOrThrow("DATABASE_NAME");
+        String pass = getValueOrThrow("DATABASE_PASS");
+        String url = "jdbc:postgresql://" + getValueOrDefault("DATABASE_HOST", "localhost")
+            + getValueOrDefault("DATABASE_PORT", "5433") + "/" + getValueOrThrow("DATABASE_NAME");
+        return new DefaultRoleSetupImportService(url,username,pass);
     }
 
-    @SuppressWarnings({"deprecation"})
-    public DataSource createDataSource() {
-        PGPoolingDataSource dataSource = new PGPoolingDataSource();
-        dataSource.setServerName(getValueOrDefault("DATABASE_HOST", "localhost"));
-        dataSource.setPortNumber(Integer.parseInt(getValueOrDefault("DATABASE_PORT", "5433")));
-        dataSource.setDatabaseName(getValueOrThrow("DATABASE_NAME"));
-        dataSource.setUser(getValueOrThrow("DATABASE_USER"));
-        dataSource.setPassword(getValueOrThrow("DATABASE_PASS"));
-        dataSource.setMaxConnections(25);
-        return dataSource;
-    }
+
 
     public static String getValueOrDefault(String name, String defaultValue) {
         String value = getenv(name);

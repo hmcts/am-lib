dbae9590c3fb2a3dd6ad97209c043284128bcdf0
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-07 16:20:19 +0100

    test:Am-338 load/delete function test data serenity

--- am-lib/src/main/java/uk/gov/hmcts/reform/amlib/internal/aspects/ErrorHandlingAspect.java
+++ am-lib/src/main/java/uk/gov/hmcts/reform/amlib/internal/aspects/ErrorHandlingAspect.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.amlib.internal.aspects;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,6 +8,7 @@ import org.jdbi.v3.core.JdbiException;
 import uk.gov.hmcts.reform.amlib.exceptions.PersistenceException;
 
 @Aspect
+@Slf4j
 public class ErrorHandlingAspect {
 
     @Around("execution(public * uk.gov.hmcts.reform.amlib.*Service.*(..))")
@@ -14,6 +16,7 @@ public class ErrorHandlingAspect {
         try {
             return joinPoint.proceed();
         } catch (JdbiException ex) {
+            log.info("JdbiException Exception in aspect:::"+ex.getMessage());
             throw new PersistenceException(ex);
         }
     }

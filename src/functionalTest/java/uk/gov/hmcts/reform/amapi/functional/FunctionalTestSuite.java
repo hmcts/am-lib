cf15eb5ba236180d24a1395a3f77fd92c56abfe0
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-09 20:37:29 +0100

    test: serenity test data load

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/FunctionalTestSuite.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/FunctionalTestSuite.java
@@ -1,26 +1,12 @@
 package uk.gov.hmcts.reform.amapi.functional;
 
 import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
-import uk.gov.hmcts.reform.amapi.functional.conf.SerenityBeanConfiguration;
 import uk.gov.hmcts.reform.amapi.functional.client.AmApiClient;
-import uk.gov.hmcts.reform.amlib.DefaultRoleSetupImportService;
-import uk.gov.hmcts.reform.amlib.models.ResourceDefinition;
-
-import static uk.gov.hmcts.reform.amlib.enums.AccessType.EXPLICIT;
-import static uk.gov.hmcts.reform.amlib.enums.RoleType.RESOURCE;
-import static uk.gov.hmcts.reform.amlib.enums.SecurityClassification.PUBLIC;
 
 @TestPropertySource("classpath:application-functional.yaml")
-@Import(SerenityBeanConfiguration.class)
 public class FunctionalTestSuite {
-
-
-    @Autowired
-    private DefaultRoleSetupImportService importerService;
 
     @Value("${targetInstance}")
     protected String accessUrl;
@@ -30,13 +16,7 @@ public class FunctionalTestSuite {
     @Before
     public void setUp() {
         amApiClient = new AmApiClient(accessUrl);
-        importerService.addService("cmc-test");
 
-        ResourceDefinition resourceDefinition = ResourceDefinition.builder()
-            .serviceName("cmc-test").resourceType("case-test").resourceName("claim-test").build();
-        importerService.addResourceDefinition(resourceDefinition);
-        importerService.addRole("caseworker-test", RESOURCE, PUBLIC, EXPLICIT);
-        importerService.getService("cmc-test"); // verify service inserted or not
     }
 
 }

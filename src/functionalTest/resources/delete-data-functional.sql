94d857b88089a26dc46c511442e259ff8f02b747
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-13 16:20:45 +0100

    Revert "test: serenity test data load"
    This reverts commit b245cb2e872b99295a4166443dfd5aa26d457e82.

--- src/functionalTest/resources/delete-data-functional.sql
+++
@@ -1,4 +0,0 @@
-delete from access_management where service_name = 'cmc-test' and resource_type ='case-test' and resource_name = 'claim-test';
-delete from roles where role_name ='caseworker-test';
-delete from resources where service_name = 'cmc-test' and resource_type ='case-test' and resource_name = 'claim-test';
-delete from services where service_name = 'cmc-test';
--- src/functionalTest/resources/load-data-functional.sql
+++
@@ -1,5 +0,0 @@
-insert into services (service_name, service_description) values ('cmc-test', null);
-
-insert into resources (service_name, resource_type, resource_name) values ('cmc-test', 'case-test', 'claim-test');
-
-insert into roles (role_name, role_type, security_classification, access_type) values ('caseworker-test', cast('RESOURCE' as role_type),cast('PUBLIC' as security_classification), cast('ROLE_BASED' as access_type))

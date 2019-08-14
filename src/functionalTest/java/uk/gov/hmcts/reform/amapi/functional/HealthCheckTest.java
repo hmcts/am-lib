917e1a23561be2074d6c08f9ebb50b35fd0d1196
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-14 22:56:17 +0100

    refactor: added smoke test for master

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/HealthCheckTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.amapi.functional;
+
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.amapi.functional.client.AmApiClient;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert"})
+@TestPropertySource("classpath:application-functional.yaml")
+public class HealthCheckTest {
+
+    public AmApiClient amApiClient;
+
+    @Value("${targetInstance}")
+    protected String accessUrl;
+
+    @Test
+    @Tag("SmokeTest")
+    public void healthcheckReturns200() {
+        amApiClient = new AmApiClient(accessUrl);
+        amApiClient.buildRequest().get(amApiClient.getAccessUrl() + "health")
+            .then().statusCode(200)
+            .and().body("status", equalTo("UP"));
+    }
+
+}

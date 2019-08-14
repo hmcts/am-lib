7cc007205bfd67339df6023026e746a8c08bb3ae
abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
2019-08-14 11:43:50 +0100

    test: serenity test data load (#293)

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/FunctionalTestSuite.java
@@ -0,0 +1,104 @@
+package uk.gov.hmcts.reform.amapi.functional;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
+import org.postgresql.ds.PGPoolingDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.amapi.functional.client.AmApiClient;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+
+import static java.lang.System.getenv;
+
+@TestPropertySource("classpath:application-functional.yaml")
+@Slf4j
+@SuppressWarnings({"PMD.DataflowAnomalyAnalysis", "PMD.ConfusingTernary"})
+public class FunctionalTestSuite {
+
+    @Value("${targetInstance}")
+    protected String accessUrl;
+
+    public AmApiClient amApiClient;
+
+    @Before
+    public void setUp() throws Exception {
+        log.info("Am api rest url::" + accessUrl);
+        amApiClient = new AmApiClient(accessUrl);
+
+        String loadFile = Thread.currentThread().getContextClassLoader()
+            .getResource("load-data-functional.sql").getPath();
+        String deleteFile = Thread.currentThread().getContextClassLoader()
+            .getResource("delete-data-functional.sql").getPath();
+        List<Path> files = new ArrayList<>();
+        files.add(Paths.get(deleteFile));
+        files.add(Paths.get(loadFile));
+
+        executeScript(files);
+    }
+
+    private void executeScript(List<Path> scriptFiles) throws SQLException, IOException {
+        try (Connection connection = createDataSource().getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                for (Path path : scriptFiles) {
+                    for (String scriptLine : Files.readAllLines(path)) {
+                        statement.addBatch(scriptLine);
+                    }
+                    statement.executeBatch();
+                }
+            }
+        } catch (Exception exe) {
+            log.error("FunctionalTestSuite script execution error with script ::" + exe.toString());
+            throw exe;
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        String deleteFile = Thread.currentThread().getContextClassLoader()
+            .getResource("delete-data-functional.sql").getPath();
+        List<Path> files = new ArrayList<>();
+        files.add(Paths.get(deleteFile));
+        executeScript(files);
+    }
+
+
+    @SuppressWarnings({"deprecation"})
+    public DataSource createDataSource() {
+        log.info("DB Host name::" + getValueOrDefault("DATABASE_HOST", "localhost"));
+        PGPoolingDataSource dataSource = new PGPoolingDataSource();
+        dataSource.setServerName(getValueOrDefault("DATABASE_HOST", "localhost"));
+        dataSource.setPortNumber(Integer.parseInt(getValueOrDefault("DATABASE_PORT", "5433")));
+        dataSource.setDatabaseName(getValueOrThrow("DATABASE_NAME"));
+        dataSource.setUser(getValueOrThrow("DATABASE_USER"));
+        dataSource.setPassword(getValueOrThrow("DATABASE_PASS"));
+        dataSource.setMaxConnections(5);
+        return dataSource;
+    }
+
+
+    public static String getValueOrDefault(String name, String defaultValue) {
+        String value = getenv(name);
+        return value != null ? value : defaultValue;
+    }
+
+    public static String getValueOrThrow(String name) {
+        String value = getenv(name);
+        if (value == null) {
+            throw new IllegalArgumentException("Environment variable '" + name + "' is missing");
+        }
+        return value;
+    }
+
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/client/AmApiClient.java
@@ -0,0 +1,126 @@
+package uk.gov.hmcts.reform.amapi.functional.client;
+
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import io.restassured.specification.RequestSpecification;
+import lombok.Data;
+import net.serenitybdd.rest.SerenityRest;
+import uk.gov.hmcts.reform.amapi.models.FilterResource;
+import uk.gov.hmcts.reform.amlib.enums.Permission;
+import uk.gov.hmcts.reform.amlib.enums.SecurityClassification;
+import uk.gov.hmcts.reform.amlib.models.ExplicitAccessGrant;
+import uk.gov.hmcts.reform.amlib.models.ExplicitAccessMetadata;
+import uk.gov.hmcts.reform.amlib.models.Resource;
+import uk.gov.hmcts.reform.amlib.models.ResourceDefinition;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static uk.gov.hmcts.reform.amlib.enums.AccessorType.USER;
+import static uk.gov.hmcts.reform.amlib.enums.Permission.CREATE;
+import static uk.gov.hmcts.reform.amlib.enums.Permission.READ;
+import static uk.gov.hmcts.reform.amlib.enums.Permission.UPDATE;
+
+@Data
+@SuppressWarnings({"PMD.LawOfDemeter"})
+public class AmApiClient {
+
+    private long currentDateTime  = System.currentTimeMillis();
+    String resourceId = "resourceId" + currentDateTime;
+    @NotEmpty Set<@NotBlank String> accessorIds;
+    String accessorId = "accessorId" + currentDateTime;
+    String resourceName = "claim";
+    String resourceType = "case";
+    String seriviceName = "cmc";
+    String relationship = "caseworker";
+    Set<String> userRoles = new HashSet<>();
+    Map<JsonPointer, Set<Permission>> multipleAttributePermissions = ImmutableMap.of(
+        JsonPointer.valueOf(""), ImmutableSet.of(READ, CREATE, UPDATE));
+
+    private final String accessUrl;
+
+    public AmApiClient(String accessUrl) {
+        this.accessUrl = accessUrl;
+    }
+
+    public RequestSpecification buildRequest() {
+        return SerenityRest.given()
+            .relaxedHTTPSValidation()
+            .header("Content-Type", APPLICATION_JSON_UTF8_VALUE)
+            .header("Accepts", APPLICATION_JSON_UTF8_VALUE);
+    }
+
+    public RequestSpecification createExplicitAccess() {
+        accessorIds = new HashSet<>();
+        accessorIds.add(accessorId);
+        ExplicitAccessGrant requestBody = ExplicitAccessGrant.builder()
+            .resourceId(resourceId)
+            .accessorIds(accessorIds)
+            .accessorType(USER)
+            .resourceDefinition(ResourceDefinition.builder()
+                .resourceName(resourceName).resourceType(resourceType).serviceName(seriviceName).build())
+            .attributePermissions(multipleAttributePermissions)
+            .relationship(relationship)
+            .build();
+
+        return buildRequest().body(requestBody);
+    }
+
+    public RequestSpecification createRevokeAccess(String accessorId, String resourceId) {
+        ExplicitAccessMetadata requestBody = ExplicitAccessMetadata.builder()
+            .accessorId(accessorId)
+            .resourceId(resourceId)
+            .resourceName(resourceName)
+            .resourceType(resourceType)
+            .serviceName(seriviceName)
+            .accessorType(USER)
+            .attribute(JsonPointer.valueOf(""))
+            .relationship(relationship)
+            .build();
+
+        return buildRequest().body(requestBody);
+    }
+
+    public RequestSpecification createRevokeAccessWithoutOptionalParams(String accessorId, String resourceId) {
+        ExplicitAccessMetadata requestBody = ExplicitAccessMetadata.builder()
+            .accessorId(accessorId)
+            .resourceId(resourceId)
+            .resourceType(resourceType)
+            .accessorType(USER)
+            .attribute(JsonPointer.valueOf(""))
+            .relationship(relationship)
+            .build();
+
+        return buildRequest().body(requestBody);
+    }
+
+    public RequestSpecification createFilterAccess(String resourceId, String accessorId) {
+        JsonNode data = JsonNodeFactory.instance.objectNode().put("json", "resource");
+        userRoles.add(relationship);
+        FilterResource requestBody = FilterResource.builder()
+            .userId(accessorId)
+            .userRoles(userRoles)
+            .resource(Resource.builder()
+                .id(resourceId)
+                .definition(ResourceDefinition.builder()
+                    .resourceName(resourceName)
+                    .resourceType(resourceType)
+                    .serviceName(seriviceName)
+                    .build())
+                .data(data)
+                .build())
+            .attributeSecurityClassification(Collections.singletonMap(JsonPointer.valueOf(""),
+                SecurityClassification.PUBLIC))
+            .build();
+
+        return buildRequest().body(requestBody);
+    }
+}

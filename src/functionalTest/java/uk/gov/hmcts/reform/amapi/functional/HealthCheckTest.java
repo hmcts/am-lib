d33bc99cae706db7c50cbb3e00d8ec6f121937b5
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-15 11:52:51 +0100

    refactor: Smoke test fix master

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/HealthCheckTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/HealthCheckTest.java
@@ -1,30 +1,19 @@
 package uk.gov.hmcts.reform.amapi.functional;
 
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import org.junit.Test;
 import org.junit.jupiter.api.Tag;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import uk.gov.hmcts.reform.amapi.functional.client.AmApiClient;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.get;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-@RunWith(SpringIntegrationSerenityRunner.class)
-@SuppressWarnings({"PMD.JUnitTestsShouldIncludeAssert"})
-public class HealthCheckTest {
-
-    public AmApiClient amApiClient;
-
-    @Value("${TEST_URL:http://localhost:3704}")
-    protected String accessUrl;
+@SuppressWarnings("PMD")
+class HealthCheckTest extends RestAssuredTest {
 
     @Test
     @Tag("SmokeTest")
-    public void healthcheckReturns200() {
-        amApiClient = new AmApiClient(accessUrl);
-        amApiClient.buildRequest().get(amApiClient.getAccessUrl() + "/health")
+    void healthcheck_returns_200() {
+        get("/health")
             .then().statusCode(200)
             .and().body("status", equalTo("UP"));
     }
-
 }

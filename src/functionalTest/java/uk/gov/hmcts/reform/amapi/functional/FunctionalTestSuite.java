b11031f25d0c6eb6a66b83b4d21e62a158ecc0a3
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-16 19:43:48 +0100

    refactor: Jenkins update for S2s

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/FunctionalTestSuite.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/FunctionalTestSuite.java
@@ -51,6 +51,7 @@ public class FunctionalTestSuite {
         log.info("Configured S2S secret: " + s2sSecret.substring(0, 2) + "************" + s2sSecret.substring(14));
         log.info("Configured S2S microservice: " + s2sName);
         log.info("Configured S2S URL: " + s2sUrl);
+        log.info("access url::" + accessUrl);
 
         String s2sToken = new S2sClient(s2sUrl, s2sName, s2sSecret).signIntoS2S();
         amApiClient = new AmApiClient(accessUrl, s2sToken);

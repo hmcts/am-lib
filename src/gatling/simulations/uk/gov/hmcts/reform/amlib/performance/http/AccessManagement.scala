5175487a30d12aa49c6653329baa386838f7ea14
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-19 21:12:59 +0100

    refactor: Nightly build with S2S Authentication

--- src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/http/AccessManagement.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/http/AccessManagement.scala
@@ -28,7 +28,7 @@ object AccessManagement {
   private def getRequest(url: String): HttpRequestBuilder =
     http("/returnResourceAccessors")
       .get("/api" + url)
-      .header("ServiceAuthorization", ${auth_Response}")
+      .header("ServiceAuthorization", "${auth_Response}")
       .check(status.is(200))
 
   private def postRequest(url: String, body: String, statusExpected: Int): HttpRequestBuilder =

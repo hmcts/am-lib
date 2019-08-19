e51e40c732dc886fbfd38ef48d3b7552c89d6739
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-08-19 17:21:30 +0100

    refactor: Nightly build with S2S Authentication

--- src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/http/AccessManagement.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/http/AccessManagement.scala
@@ -2,13 +2,26 @@ package uk.gov.hmcts.reform.amlib.performance.http
 
 import java.util.UUID
 
-import io.gatling.core.Predef._
-import io.gatling.http.Predef._
-import io.gatling.http.request.builder.HttpRequestBuilder
+import com.warrenstrange.googleauth.GoogleAuthenticator
 
 object AccessManagement {
 
-  private def getRequest(url: String): HttpRequestBuilder =
+  val s2sUrl : String = scala.util.Properties.envOrElse("s2s-url","http://127.0.0.1:8502")
+
+  val s2sname : String = scala.util.Properties.envOrElse("s2s-name","am_accessmgmt_api")
+
+  val s2sSecret : String = scala.util.Properties.envOrElse(" s2s-secret","GJNMFGFAAO4FCVD4")
+
+  private val authenticator = new  GoogleAuthenticator
+
+  val params = Map("microservice" -> s2sname, "oneTimePassword" -> authenticator.getTotpPassword(s2sSecret))
+
+  http(s2sUrl)
+    .post("/lease")
+    .body(params)
+    .check(bodyString.saveAs("auth_Response"))
+
+  private def getRequest(url: String): HttpRequestBuilder =0
     http("/returnResourceAccessors")
       .get("/api" + url)
       .check(status.is(200))
@@ -16,14 +29,18 @@ object AccessManagement {
   private def postRequest(url: String, body: String, statusExpected: Int): HttpRequestBuilder =
     http(url)
       .post("/api" + url)
+      .header("${auth_Response}")
       .body(ElFileBody(body)).asJson
       .check(status.is(statusExpected))
 
   private def deleteRequest(url: String, body: String): HttpRequestBuilder =
     http(url)
       .delete("/api" + url)
+      .header("${auth_Response}")
       .body(ElFileBody(body)).asJson
       .check(status.is(204))
+
+
 
   def createResourceAssess: HttpRequestBuilder =
     postRequest("/access-resource","createResourceAccess.json", 201)

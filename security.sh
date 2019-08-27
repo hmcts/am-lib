a240283b0014b0f443d3a1655d13ef28f815705f
Benjamin Matthews <benjamin.matthews@hmcts.net>
2019-08-27 14:49:46 +0100

    feat: OWASP ZAP security scan

---
+++ security.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo ${TEST_URL}
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001 -l FAIL
+cat zap.out
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+mkdir -p functional-output
+cp /zap/api-report.html functional-output/
+chmod a+wx functional-output
+chmod a+wx functional-output/api-report.html
+zap-cli -p 1001 alerts -l High

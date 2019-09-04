70226427c5ad4f48cb515ce549b8fa83bae70d00
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-09-04 17:27:39 +0100

    feat: Versioning Api changes AM-381

--- src/main/java/uk/gov/hmcts/reform/amapi/controllers/AccessManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/amapi/controllers/AccessManagementController.java
@@ -29,7 +29,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * Default endpoints per application.
  */
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/v1")
 @SuppressWarnings({"PMD.AvoidDuplicateLiterals"})
 public class AccessManagementController {
 

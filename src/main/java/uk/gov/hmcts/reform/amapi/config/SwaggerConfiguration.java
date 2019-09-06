fb50ffd21326fefb2ef7c36c5561be36388df681
abhijit.diwan <abhijit.diwan@HMCTS.net>
2019-09-06 12:37:22 +0100

    Refactor: Added Swagger support for S2S

--- src/main/java/uk/gov/hmcts/reform/amapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/amapi/config/SwaggerConfiguration.java
@@ -4,12 +4,18 @@ import com.fasterxml.jackson.core.JsonPointer;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.amapi.Application;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,12 +23,19 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder.name("ServiceAuthorization").modelRef(new ModelRef("string"))
+            .parameterType("header").required(true).build();
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
             .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
             .paths(PathSelectors.any())
             .build().directModelSubstitute(JsonPointer.class, String.class)
-            .directModelSubstitute(JsonNode.class,String.class);
+            .globalOperationParameters(parameters)
+            .directModelSubstitute(JsonNode.class, String.class);
     }
 }

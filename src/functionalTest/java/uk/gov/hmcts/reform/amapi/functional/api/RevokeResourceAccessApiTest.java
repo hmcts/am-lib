69d61c8d94fbf871974d86b42a53f685b582d4cd
Benjamin Matthews <benjamin.matthews@hmcts.net>
2019-09-23 15:38:45 +0100

    feat: functional tests for revoke access endpoint

--- src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/api/RevokeResourceAccessApiTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/amapi/functional/api/RevokeResourceAccessApiTest.java
@@ -1,13 +1,23 @@
 package uk.gov.hmcts.reform.amapi.functional.api;
 
+import com.fasterxml.jackson.core.JsonPointer;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.gov.hmcts.reform.amapi.functional.FunctionalTestSuite;
+import uk.gov.hmcts.reform.amlib.models.AccessManagementAudit;
 import uk.gov.hmcts.reform.amlib.models.ExplicitAccessGrant;
 import uk.gov.hmcts.reform.amlib.models.ExplicitAccessMetadata;
+import uk.gov.hmcts.reform.amlib.models.ResourceDefinition;
+
+import java.time.Instant;
+
+import static uk.gov.hmcts.reform.amlib.enums.AccessorType.ROLE;
+import static uk.gov.hmcts.reform.amlib.enums.Permission.READ;
 
 @Slf4j
 @RunWith(SpringIntegrationSerenityRunner.class)
@@ -44,6 +54,94 @@ public class RevokeResourceAccessApiTest extends FunctionalTestSuite {
             .accessorType(accessorType)
             .attribute(attribute)
             .relationship(relationship)
+            .resourceType(resourceType)
+            .build();
+
+        Response response = createThenRevokeAccess(explicitAccessGrant, explicitAccessMetadata);
+        response.then().statusCode(204);
+        response.then().log();
+    }
+
+    @Test
+    public void verifyRevokeExplicitAccessApiWithIncorrectResourceNameAndServiceName() {
+
+        ExplicitAccessGrant explicitAccessGrant = createGenericExplicitAccessGrant();
+        ExplicitAccessMetadata explicitAccessMetadata = ExplicitAccessMetadata.builder()
+            .resourceId(resourceId)
+            .accessorId(accessorId)
+            .accessorType(accessorType)
+            .attribute(attribute)
+            .relationship(relationship)
+            .serviceName("some service")
+            .resourceName("some resource")
+            .resourceType(resourceType)
+            .build();
+
+        Response response = createThenRevokeAccess(explicitAccessGrant, explicitAccessMetadata);
+        response.then().statusCode(204);
+        response.then().log();
+    }
+
+    @Test
+    public void verifyRevokeExplicitAccessApiForRole() {
+
+        ExplicitAccessGrant explicitAccessGrant = ExplicitAccessGrant.builder()
+            .resourceId(resourceId)
+            .resourceDefinition(ResourceDefinition.builder()
+                .serviceName(serviceName)
+                .resourceName(resourceName)
+                .resourceType(resourceType)
+                .lastUpdate(Instant.now())
+                .build())
+            .accessorIds(ImmutableSet.of(accessorId))
+            .accessorType(ROLE)
+            .attributePermissions(ImmutableMap.of(JsonPointer.valueOf(""), ImmutableSet.of(READ)))
+            .relationship(relationship)
+            .accessManagementAudit(AccessManagementAudit.builder()
+                .lastUpdate(Instant.now())
+                .build())
+            .build();
+        ExplicitAccessMetadata explicitAccessMetadata = ExplicitAccessMetadata.builder()
+            .resourceId(resourceId)
+            .accessorId(accessorId)
+            .accessorType(ROLE)
+            .attribute(attribute)
+            .relationship(relationship)
+            .serviceName(serviceName)
+            .resourceName(resourceName)
+            .resourceType(resourceType)
+            .build();
+
+        Response response = createThenRevokeAccess(explicitAccessGrant, explicitAccessMetadata);
+        response.then().statusCode(204);
+        response.then().log();
+    }
+
+    @Test
+    public void verifyRevokeExplicitAccessApiForUserWithNullRelationship() {
+
+        ExplicitAccessGrant explicitAccessGrant = ExplicitAccessGrant.builder()
+            .resourceId(resourceId)
+            .resourceDefinition(ResourceDefinition.builder()
+                .serviceName(serviceName)
+                .resourceName(resourceName)
+                .resourceType(resourceType)
+                .lastUpdate(Instant.now())
+                .build())
+            .accessorIds(ImmutableSet.of(accessorId))
+            .accessorType(accessorType)
+            .attributePermissions(ImmutableMap.of(JsonPointer.valueOf(""), ImmutableSet.of(READ)))
+            .accessManagementAudit(AccessManagementAudit.builder()
+                .lastUpdate(Instant.now())
+                .build())
+            .build();
+        ExplicitAccessMetadata explicitAccessMetadata = ExplicitAccessMetadata.builder()
+            .resourceId(resourceId)
+            .accessorId(accessorId)
+            .accessorType(ROLE)
+            .attribute(attribute)
+            .serviceName(serviceName)
+            .resourceName(resourceName)
             .resourceType(resourceType)
             .build();
 

b309e636ae7fdf7d526aca5455d72fdd869dd03b
abhijit <Abhijit.diwan@hmcts.net>
2019-10-09 21:07:21 +0100

    fix: refactor Audit

--- am-lib/src/main/resources/db/migration/V21__Audit_Scripts_AM.sql
+++ am-lib/src/main/resources/db/migration/V21__Audit_Scripts_AM.sql
@@ -5,17 +5,17 @@ create table access_management_audit
     access_management_id    BIGINT            not null,
     resource_id             VARCHAR(250)      not null,
     accessor_id             VARCHAR(100)      not null,
-    permissions             integer  		      not null,
+    permissions             INTEGER  		  not null,
     accessor_type           accessor_type     not null,
     service_name            VARCHAR(100)      not null,
     resource_type           VARCHAR(100)      not null,
     resource_name           VARCHAR(100)      not null,
     attribute               VARCHAR(250)      not null,
     relationship            VARCHAR(100),
-	  calling_service_name    VARCHAR(100),
-    audit_timestamp		      TIMESTAMP 		    not null DEFAULT (now() at time zone 'utc'),
+	  calling_service_name  VARCHAR(100),
+    audit_timestamp		    TIMESTAMP 	      not null DEFAULT (now() at time zone 'utc'),
     changed_by              VARCHAR(100),
-    action                  ACTION
+    action                  ACTION            not null
 );
 
 create table default_permissions_for_roles_audit
@@ -25,11 +25,11 @@ create table default_permissions_for_roles_audit
     resource_name 			    VARCHAR(100)       not null,
     attribute     			    VARCHAR(250)       not null,
     role_name     			    VARCHAR(100)       not null,
-    permissions   			    SMALLINT  		     not null,
-	  calling_service_name    VARCHAR(100),
-    audit_timestamp		      TIMESTAMP 		    not null DEFAULT (now() at time zone 'utc'),
-    changed_by              VARCHAR(100),
-    action                  ACTION
+    permissions   			    SMALLINT  		   not null,
+	  calling_service_name      VARCHAR(100),
+    audit_timestamp		        TIMESTAMP 		   not null DEFAULT (now() at time zone 'utc'),
+    changed_by                  VARCHAR(100),
+    action                      ACTION             not null
 );
 
 create table resource_attributes_audit
@@ -39,8 +39,8 @@ create table resource_attributes_audit
     resource_name                   VARCHAR(100)            not null,
     attribute                       VARCHAR(250)            not null,
     default_security_classification SECURITY_CLASSIFICATION not null,
-	  calling_service_name            VARCHAR(100),
-    audit_timestamp		              TIMESTAMP 		    not null DEFAULT (now() at time zone 'utc'),
+	calling_service_name            VARCHAR(100),
+    audit_timestamp		            TIMESTAMP 		        not null DEFAULT (now() at time zone 'utc'),
     changed_by                      VARCHAR(100),
-    action                          ACTION
+    action                          ACTION                  not null
 );

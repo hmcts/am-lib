e7b085222df739843d7f80cc48f14b59db05a2c7
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2019-10-10 11:37:36 +0100

    AM-414-filter-resource-functional-test: initial commit

--- src/functionalTest/resources/load-data-functional.sql
+++ src/functionalTest/resources/load-data-functional.sql
@@ -4,3 +4,4 @@ insert into resources (service_name, resource_type, resource_name) values ('cmc-
 
 insert into roles (role_name, role_type, security_classification, access_type) values ('caseworker-test', cast('RESOURCE' as role_type),cast('PUBLIC' as security_classification), cast('ROLE_BASED' as access_type));
 
+insert into roles (role_name, role_type, security_classification, access_type) values ('caseworker-test1', cast('IDAM' as role_type),cast('PUBLIC' as security_classification), cast('ROLE_BASED' as access_type));

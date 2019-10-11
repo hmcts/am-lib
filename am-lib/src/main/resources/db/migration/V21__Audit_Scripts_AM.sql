e2c7329ccd284030af97ee8a8647508b566eb882
abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
2019-10-11 16:49:18 +0100

    fix: flyway failure fix for audit table (#425)

--- am-lib/src/main/resources/db/migration/V21__Audit_Scripts_AM.sql
+++ am-lib/src/main/resources/db/migration/V21__Audit_Scripts_AM.sql
@@ -5,17 +5,17 @@ create table access_management_audit
     access_management_id    BIGINT            not null,
     resource_id             VARCHAR(250)      not null,
     accessor_id             VARCHAR(100)      not null,
-    permissions             INTEGER  		  not null,
+    permissions             integer  		      not null,
     accessor_type           accessor_type     not null,
     service_name            VARCHAR(100)      not null,
     resource_type           VARCHAR(100)      not null,
     resource_name           VARCHAR(100)      not null,
     attribute               VARCHAR(250)      not null,
     relationship            VARCHAR(100),
-	  calling_service_name  VARCHAR(100),
-    audit_timestamp		    TIMESTAMP 	      not null DEFAULT (now() at time zone 'utc'),
+	calling_service_name    VARCHAR(100),
+    audit_timestamp		    TIMESTAMP 		  not null DEFAULT (now() at time zone 'utc'),
     changed_by              VARCHAR(100),
-    action                  ACTION            not null
+    action                  ACTION
 );
 
 create table default_permissions_for_roles_audit
@@ -26,10 +26,10 @@ create table default_permissions_for_roles_audit
     attribute     			    VARCHAR(250)       not null,
     role_name     			    VARCHAR(100)       not null,
     permissions   			    SMALLINT  		   not null,
-	  calling_service_name      VARCHAR(100),
+	calling_service_name        VARCHAR(100),
     audit_timestamp		        TIMESTAMP 		   not null DEFAULT (now() at time zone 'utc'),
     changed_by                  VARCHAR(100),
-    action                      ACTION             not null
+    action                      ACTION
 );
 
 create table resource_attributes_audit
@@ -42,5 +42,5 @@ create table resource_attributes_audit
 	calling_service_name            VARCHAR(100),
     audit_timestamp		            TIMESTAMP 		        not null DEFAULT (now() at time zone 'utc'),
     changed_by                      VARCHAR(100),
-    action                          ACTION                  not null
+    action                          ACTION
 );

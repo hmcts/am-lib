f8760acf5deace2d7e949531a39a85e6d2e98925
abhijit <Abhijit.diwan@hmcts.net>
2019-10-22 10:23:44 +0100

    fix: zap security fix glue failures

--- security.sh
+++ security.sh
@@ -8,5 +8,6 @@ export LANG=C.UTF-8
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 mkdir -p functional-output
 chmod a+wx functional-output
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp /zap/api-report.html functional-output/
-zap-cli -p 1001 alerts -l High
+zap-cli -p 1001 alerts -l High alerts -l Informational --exit-code False
